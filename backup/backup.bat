79a32d2ed7df4cec992002e287cf4c7c3fc6404c
ser4ee for re3v76 <81680567+ser4ee@users.noreply.github.com>
2021-07-02 14:39:28 +0300

    Add files via upload

---
+++ backup/backup.bat
@@ -0,0 +1,19 @@
+@echo off
+echo ====================================================
+echo Files are archived and copied.
+echo With respect,re3v76.
+echo Githab link http://
+echo ====================================================
+set day=%DATE:~0,2%
+set month=%DATE:~3,2%
+set year=%DATE:~6,4%
+set wr=C:\Program Files\WinRAR\WinRaR.exe 
+:: C:\Program Files\WinRAR\WinRAR.exe - the path to the archiver.
+set backup=C:\backup\bp_r_%day%_%month%_%year%.zip
+:: C:\buckap\backup_repo_%day%_%month%_%year%.zip the backup path and the file name. Your own derektoriya.
+set file=G:\repo
+:: G:\repo that's what we're backing up.
+"%wr%" a -r -m5 %backup% %file%
+
+
+
